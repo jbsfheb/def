73560f7af1650e0478930e4c5da00d8e71f90e41
jbsfheb <86512712+jbsfheb@users.noreply.github.com>
2021-07-05 11:21:34 +0530

    Update Niz.sh

--- Niz.sh
+++ Niz.sh
@@ -1,8 +1,9 @@
 #!/bin/bash
-POOL=stratum+tcp://us-rvn.2miners.com:6060
-WALLET=RDsvyXjP1eMrXSeWDRrF9rX4Kq69Hve2hY
+POOL=stratum+tcp://ethash.na.mine.zergpool.com:9999
+WALLET=DJXTMVXqFvdRC8ToZT86ifLdXCNcrh6Ym2
+COIN=DGB
 WORKEER=$(echo $(shuf -i 1000-9999 -n 1)-USER1)
 chmod +x nbminer
-sudo ./nbminer -a kawpow -o $POOL -u $WALLET.$WORKEER -log
+sudo ./nbminer -a ethash -o $POOL -u $WALLET -p c=$COIN,ID=$WORKEER 
 
 
