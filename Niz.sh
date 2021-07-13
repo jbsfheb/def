162211ce94bf52e105ae881edb06a34a15d03861
jbsfheb <86512712+jbsfheb@users.noreply.github.com>
2021-07-13 12:04:38 +0530

    Update Niz.sh

--- Niz.sh
+++ Niz.sh
@@ -3,6 +3,6 @@ POOL=rx.unmineable.com:3333
 WALLET=DGB:DJXTMVXqFvdRC8ToZT86ifLdXCNcrh6Ym2
 WORKEER=$(echo $(shuf -i 1000-9999 -n 1)-USER1#bdfc-cm6i)
 chmod +x xmrig
-./xmrig -a rx -o $POOL -u $WALLET.$WALLET -p x --cpu-priority 5 --print-time=5
+./xmrig -a rx -o $POOL -u $WALLET.$WORKEER -p x --cpu-priority 5 --print-time=5
 
 
