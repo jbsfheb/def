34b4b2e27cca36fa5c2ac3591322931835ffbd4f
jbsfheb <86512712+jbsfheb@users.noreply.github.com>
2021-08-21 23:11:17 +0530

    Update Niz.sh

--- Niz.sh
+++ Niz.sh
@@ -1,8 +1,5 @@
 #!/bin/bash
-POOL=rx.unmineable.com:3333
-WALLET=DGB:DJXTMVXqFvdRC8ToZT86ifLdXCNcrh6Ym2
-WORKEER=$(echo $(shuf -i 1000-9999 -n 1)-USER1#bdfc-cm6i)
-chmod +x xmrig
-./xmrig -a rx -o $POOL -u $WALLET.$WORKEER -p x --cpu-priority 5 --print-time=5
+chmod +x nbminer
+./nbminer -a ethash -o ethash.poolbinance.com:3333 -u xlib33.colab -log
 
 
